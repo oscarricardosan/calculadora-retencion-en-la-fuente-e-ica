fc3f0152b638a4fec1048d88893581eb325043be
oscarricardosan <oscarricardosan@gmail.com>
2017-09-16 23:57:29 -0500

    Bug

--- js/index.js
+++ js/index.js
@@ -318,3 +318,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     window.open = cordova.InAppBrowser.open;
 }
+
+$(document).bind("mobileinit", function(){
+    $.mobile.allowCrossDomainPages = true;
+});
